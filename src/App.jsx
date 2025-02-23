f53e15919987b808b93248de4a709e3fd73b5838
Ntokozokate <mangenakate@gmail.com>
2025-02-23 21:55:15 +0200

    signatories

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import Weather from "./Weather";
 
 export default function App() {
@@ -5,6 +6,10 @@ export default function App() {
     <div className="App">
       <h1>Week 3 Homework</h1>
       <Weather city="Gweru" />
+      <h1 style={{ fontSize: "12px" }}>
+        This project was coded by{" "}
+        <a href="https://github.com/Ntokozokate/week4-react-app">Ntokozo</a>
+      </h1>
     </div>
   );
 }
